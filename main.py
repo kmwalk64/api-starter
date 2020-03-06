842cbdb30b40b4196cd5f216663af22fe80b725c
Keegan <60404080+kmwalk64@users.noreply.github.com>
2020-03-06 13:16:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import requests
 
 URL = "http://api.open-notify.org/astros.json"
-
+data = {
+  "message": "success",
+  "number": 3,
+  "people": [
+    {
+      "name": "Andrew Morgan",
+      "craft": "ISS"
+    },
+    {
+      "name": "Oleg Skripochka",
+      "craft": "ISS"
+    },
+    {
+      "name": "Jessica Meir",
+      "craft": "ISS"
+    },
+  ]
+}
 res = requests.get(URL) # get the data
 res = res.json() # convert data to Python format
 
-print(res)
+print(data["message"])
